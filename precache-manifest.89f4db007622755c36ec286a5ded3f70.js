8dec332d46e32f0149b349fcb23582e02fd6493b
Pritesh Jha <priteshjha4u@gmail.com>
2019-01-11 21:19:05 +0530

    Updates

---
+++ precache-manifest.89f4db007622755c36ec286a5ded3f70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "544b8708407114da2923",
+    "url": "/react-virtual-keyboard/static/js/runtime~main.544b8708.js"
+  },
+  {
+    "revision": "8a0bc97ad9dc1939a78b",
+    "url": "/react-virtual-keyboard/static/js/main.8a0bc97a.chunk.js"
+  },
+  {
+    "revision": "c28d81eef11479f6e026",
+    "url": "/react-virtual-keyboard/static/js/1.c28d81ee.chunk.js"
+  },
+  {
+    "revision": "8a0bc97ad9dc1939a78b",
+    "url": "/react-virtual-keyboard/static/css/main.f6d5d72b.chunk.css"
+  },
+  {
+    "revision": "c28d81eef11479f6e026",
+    "url": "/react-virtual-keyboard/static/css/1.672d55a1.chunk.css"
+  },
+  {
+    "revision": "44d48d8c5a1fb51d84dba0955e4abb12",
+    "url": "/react-virtual-keyboard/index.html"
+  }
+];
