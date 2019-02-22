5993058a47bfa0a2c1bd00e88298ec739be82646
Pritesh Jha <priteshjha4u@gmail.com>
2019-02-22 18:59:11 +0530

    Updates

--- precache-manifest.a8fe16a7435446719f631a13f1dab5bd.js
+++ precache-manifest.a8fe16a7435446719f631a13f1dab5bd.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/react-virtual-keyboard/static/js/runtime~main.544b8708.js"
   },
   {
-    "revision": "5789127586b62249aeb2",
-    "url": "/react-virtual-keyboard/static/js/main.57891275.chunk.js"
+    "revision": "b840bb0e8d3a779f19d6",
+    "url": "/react-virtual-keyboard/static/js/main.b840bb0e.chunk.js"
   },
   {
     "revision": "aa158b9f6618a7ff1e78",
     "url": "/react-virtual-keyboard/static/js/1.aa158b9f.chunk.js"
   },
   {
-    "revision": "5789127586b62249aeb2",
+    "revision": "b840bb0e8d3a779f19d6",
     "url": "/react-virtual-keyboard/static/css/main.5cd71eeb.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/react-virtual-keyboard/static/css/1.672d55a1.chunk.css"
   },
   {
-    "revision": "6698d7c3686c46bfc1431333ec6a100f",
+    "revision": "0232bf9424b2f6d6c67265c499b64dd4",
     "url": "/react-virtual-keyboard/index.html"
   }
 ];
